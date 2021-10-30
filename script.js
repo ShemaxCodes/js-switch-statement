342f100c1d6cb429429b18f000720dc0a3502e6b
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2021-10-30 14:14:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-const day = "monday";
+const day = "thursday";
 switch(day) {
     case 'monday': // day === 'monday'
         console.log('Plan course structure');
         console.log('Go to coding meetup');
-        break
+        break //code will continue to execute without this
     case 'tuesday': 
         console.log('Prepare theory videos');
         break
@@ -21,4 +21,24 @@ switch(day) {
         break
     default:
         console.log('Not a valid day');   
+}
+
+// CODING CHALLENGE USING IF/ELSE
+
+const day1 = "shema";
+
+
+if(day1 === "monday") {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if(day1 === "tuesday") {
+    console.log('Prepare theory videos');
+} else if(day1 === "wednesday" || day1 === "thursday") {
+    console.log('Write code examples');
+} else if(day1 === "friday") {
+    console.log('Record videos');
+} else if(day1 === "saturday" || day1 === "sunday") {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day');
 }
